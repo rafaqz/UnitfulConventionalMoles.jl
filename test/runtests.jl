39035ba04834fbd0779e049c8a2b4e936e65fa7a
Rafael Schouten <rafaelschouten@gmail.com>
2020-07-24 23:41:48 +1000

    fix Test package

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,4 @@
-using UnitfulConventionalMoles
-using Unitful
-using Base.Test
+using UnitfulConventionalMoles, Unitful, Test
 
 @testset "conversion to grams" begin
     @test uconvert(u"g", 2u"molH") == 2.016u"g"
